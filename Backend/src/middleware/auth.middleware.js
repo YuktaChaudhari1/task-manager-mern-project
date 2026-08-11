0ed976a30d869cb30aaffd144debba72c30959f8
Yukta Chaudhari <your-github-email@example.com>
2026-08-11 16:53:33 +0530

    Fix JWT middleware

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,13 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 async function middleware(req, res, next) {
-  const authHeader = req.cookies.token || req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json("token is missing, Please  login");
+  let token;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
+  } else {
+    token = req.cookies.token;
   }
 
-  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      message: "Token is missing, please login",
+    });
+  }
 
   try {
     const decode = jwt.verify(token, process.env.JWT_SECRET);
@@ -15,7 +23,10 @@ async function middleware(req, res, next) {
     req.user = decode;
     next();
   } catch (error) {
-    return res.status(403).json({ message: "Invalid or expired token" });
+    return res.status(403).json({
+      message: "Invalid or expired token",
+    });
   }
 }
+
 module.exports = middleware;
